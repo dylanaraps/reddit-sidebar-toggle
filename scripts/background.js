ed18877fdf48121b2a71d67d044ab70b69aeb34e
Dylan Araps <dylan.araps@gmail.com>
2017-07-11 01:07:38 +1000

    general: Add toolbar button.

--- scripts/background.js
+++ scripts/background.js
@@ -1,15 +1,8 @@
 // Reddit Sidebar Toggle.
 // Created by Dylan Araps.
 
-browser.contextMenus.create({
-    id: "reddit-st",
-    title: "Reddit: Hide sidebar"
-});
-
-browser.contextMenus.onClicked.addListener(function(info) {
-    if (info.menuItemId == "reddit-st") {
-        browser.tabs.executeScript({
-            file: "scripts/reddit.js"
-        });
-    }
+browser.browserAction.onClicked.addListener(function() {
+    browser.tabs.executeScript({
+        file: "scripts/reddit.js"
+    });
 });
